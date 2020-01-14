9a77a81614777c6ce0d9ff0e9159af961de374a8
StyxS <mail@styxs.cn>
2020-01-14 22:00:41 +0800

    feature: 数据库从H2切换为MySQL

--- src/main/java/cn/styxs/personalweb/model/BaseModel.java
+++ src/main/java/cn/styxs/personalweb/model/BaseModel.java
@@ -3,6 +3,8 @@ package cn.styxs.personalweb.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -16,7 +18,9 @@ public class BaseModel {
     @GeneratedValue
     private Long id;
     @Column(name = "gmt_created", updatable = false)
+    @CreationTimestamp
     private Date createTime;
     @Column(name = "gmt_modified")
+    @UpdateTimestamp
     private Date modifiedTime;
 }
