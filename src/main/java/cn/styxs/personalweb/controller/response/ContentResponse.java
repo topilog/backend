156ff4592b8f7eee673950b2e714bc235cf0fba7
StyxS <mail@styxs.cn>
2020-01-27 23:47:34 +0800

    change: ArticleController的相关API结果塞入ContentResponse

--- src/main/java/cn/styxs/personalweb/controller/response/ContentResponse.java
+++ src/main/java/cn/styxs/personalweb/controller/response/ContentResponse.java
@@ -1,4 +1,12 @@
 package cn.styxs.personalweb.controller.response;
 
-public class ContentResponse {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class ContentResponse<T> extends BaseResponse {
+    T content;
 }
--- src/main/java/cn/styxs/personalweb/controller/rest/ArticleController.java
+++ src/main/java/cn/styxs/personalweb/controller/rest/ArticleController.java
@@ -2,6 +2,7 @@ package cn.styxs.personalweb.controller.rest;
 
 import cn.styxs.personalweb.annotation.LoginRequired;
 import cn.styxs.personalweb.controller.request.ArticlePostRequest;
+import cn.styxs.personalweb.controller.response.ContentResponse;
 import cn.styxs.personalweb.model.ArticleContent;
 import cn.styxs.personalweb.model.ArticleInfo;
 import cn.styxs.personalweb.service.ArticleService;
@@ -22,40 +23,52 @@ public class ArticleController {
 
     @RequestMapping(path = "/infos", method = {RequestMethod.GET})
     @ApiOperation(value = "拉取所有文章信息", notes = "不包含文章内容信息")
-    public List<ArticleInfo> listArticleInfos() {
-        return articleService.getArticleInfos();
+    public ContentResponse listArticleInfos() {
+        ContentResponse<List<ArticleInfo>> response = new ContentResponse<>();
+        response.setContent(articleService.getArticleInfos());
+        response.succeed();
+        return response;
     }
 
     @RequestMapping(path = "/info", method = {RequestMethod.GET})
     @ApiOperation(value = "获取某篇文章信息", notes = "不包含文章内容信息")
     @ApiImplicitParam(name = "articleId", value = "文章id", required = true)
-    public ArticleInfo getArticleInfo(@RequestParam(value = "articleId", defaultValue = "-1") Long articleId) {
+    public ContentResponse getArticleInfo(@RequestParam(value = "articleId", defaultValue = "-1") Long articleId) {
+        ContentResponse<ArticleInfo> response = new ContentResponse<>();
         if (articleId == -1)
-            return null;
+            response.failed("do not have articleId", 1);
         else {
-            return articleService.getArticleInfo(articleId);
+            response.setContent(articleService.getArticleInfo(articleId));
+            response.succeed();
         }
+        return response;
     }
 
     @RequestMapping(path = "/article", method = {RequestMethod.POST})
     @LoginRequired
     @ApiOperation(value = "提交新文章")
-    public ArticleInfo postArticle(@RequestBody ArticlePostRequest postRequest) {
+    public ContentResponse postArticle(@RequestBody ArticlePostRequest postRequest) {
         ArticleInfo info = postRequest.make();
         info.setTags(articleService.convertTags(postRequest.getTags()));
         articleService.addArticleInfo(info);
-        return info;
+        ContentResponse<ArticleInfo> contentResponse = new ContentResponse<>();
+        contentResponse.setContent(info);
+        contentResponse.succeed();
+        return contentResponse;
     }
 
     @RequestMapping(path = "/article", method = {RequestMethod.GET})
     @ApiOperation(value = "获取文章具体内容", notes = "Markdown格式内容")
     @ApiImplicitParam(name = "articleId", value = "文章id", required = true)
-    public ArticleContent getArticleContent(@RequestParam(value = "articleId", defaultValue = "-1") Long articleId) {
+    public ContentResponse getArticleContent(@RequestParam(value = "articleId", defaultValue = "-1") Long articleId) {
+        ContentResponse<ArticleContent> response = new ContentResponse<>();
         if (articleId == -1)
-            return null;
+            response.failed("do not have articleId", 1);
         else {
-            return articleService.getArticleInfo(articleId).getArtContent();
+            response.setContent(articleService.getArticleInfo(articleId).getArtContent());
+            response.succeed();
         }
+        return response;
     }
 
 }
