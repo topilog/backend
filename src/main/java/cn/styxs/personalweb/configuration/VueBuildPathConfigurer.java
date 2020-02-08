dcf2f22f84ca459a6755b8a69e376ecb0e804a32
Styx-S <mail@styxs.cn>
2020-02-08 17:09:25 +0800

    feature: 尝试解决跨域问题

--- src/main/java/cn/styxs/personalweb/configuration/VueBuildPathConfigurer.java
+++ src/main/java/cn/styxs/personalweb/configuration/VueBuildPathConfigurer.java
@@ -2,6 +2,7 @@ package cn.styxs.personalweb.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,9 +13,13 @@ public class VueBuildPathConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        if (debug) {
-            registry.addResourceHandler("/**").addResourceLocations("classpath:/vue/");
-        }
         registry.addResourceHandler("/**").addResourceLocations("classpath:/vue/dist");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        if (debug) {
+            registry.addMapping("/**");
+        }
     }
 }
